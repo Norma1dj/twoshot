b2152b8420c7f281077f6c23f7ef0ec7c8b08a3f
Drew Norman-Meadows <norma1dj@gmail.com>
2023-06-02 11:23:04 -0700

    Feature 12 Complete

--- receipts/views.py
+++ receipts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from receipts.models import Receipt
+from receipts.models import Receipt, ExpenseCategory, Account
 from django.contrib.auth.decorators import login_required
 from receipts.forms import CreateReceiptForm
 # Create your views here.
@@ -15,6 +15,7 @@ def receipt_list(request):
 
 @login_required
 def create_receipt(request):
+
     if request.method == "POST":
 
         form = CreateReceiptForm(request.POST)
@@ -31,4 +32,21 @@ def create_receipt(request):
     context = {
         "form": form,
     }
-    return render(request, "create.html", context)
+    return render(request, "create_receipt.html", context)
+
+
+@login_required()
+def category_list(request):
+    categories = ExpenseCategory.objects.filter(owner=request.user)
+    context = {
+        'categories': categories
+    }
+    return render(request, "category_list.html", context)
+
+
+def account_list(request):
+    accounts = Account.objects.filter(owner=request.user)
+    context = {
+        'accounts': accounts
+    }
+    return render(request, "account_list.html", context)
